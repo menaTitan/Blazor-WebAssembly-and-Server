08fb739748aa95f6d2615631af46882340b7e985
LOCALGOVCORP\Menas <menas@localgovcorp.com>
2024-08-02 15:03:41 -0500

    Setup and Scaffold Database

--- BookStoreApp/BookStoreApp.API/Program.cs
+++ BookStoreApp/BookStoreApp.API/Program.cs
@@ -1,11 +1,15 @@
+using BookStoreApp.API.Data;
+using Microsoft.EntityFrameworkCore;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var connString = builder.Configuration.GetConnectionString("BookStoreDbConnection");
+builder.Services.AddDbContext<BookStoreDbContext>(options => options.UseSqlServer(connString));
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+//Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
