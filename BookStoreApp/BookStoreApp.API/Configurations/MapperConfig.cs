8414b137ba08fdfd9d5eaa795fde1177339bc5f8
LOCALGOVCORP\Menas <menas@localgovcorp.com>
2024-08-16 12:45:17 -0500

    added the book Book controller

--- BookStoreApp/BookStoreApp.API/Configurations/MapperConfig.cs
+++ BookStoreApp/BookStoreApp.API/Configurations/MapperConfig.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using BookStoreApp.API.Data;
 using BookStoreApp.API.Models.Author;
+using BookStoreApp.API.Models.Book;
 
 namespace BookStoreApp.API.Configurations
 {
@@ -11,7 +12,15 @@ namespace BookStoreApp.API.Configurations
             CreateMap<AuthorCreateDto, Author>().ReverseMap();
             CreateMap<AuthorDto, Author>().ReverseMap();
             CreateMap<AuthorUpdateDto, Author>().ReverseMap();
+            CreateMap<BookCreateDto, Book>().ReverseMap();
+            CreateMap<BookUpdateDto, Book>().ReverseMap();
 
+            CreateMap<Book, BookReadOnlyDto>().ForMember(q => q.AutherName,
+                d => d.MapFrom(map => $"{map.Author.FirstName} {map.Author.LastName}"));
+
+            CreateMap<Book, BookDetailsDto>()
+                .ForMember(q => q.AuthorName, d => d.MapFrom(map => $"{map.Author.FirstName} {map.Author.LastName}"))
+                .ReverseMap();
         }
     }
 }
---
+++ BookStoreApp/BookStoreApp.API/Models/Book/BookReadOnlyDto.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authentication;
+
+namespace BookStoreApp.API.Models.Book
+{
+    public class BookReadOnlyDto
+    {
+        public string Title { get; set; }
+
+        public int? Year { get; set; }
+
+        public string Summary { get; set; } 
+
+        public string Image { get; set; } 
+
+        public double? Price { get; set; }
+
+        public int AuthorId { get; set; }
+        public string AutherName { get; set; }
+      
+    }
+}
